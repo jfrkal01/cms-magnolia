6673c8bbc856fc6959e83c4a12ce1ced6911c65c
jiri.frkal <jiri.frkal@magnolia-cms.com>
2025-01-10 10:20:02 +0100

    Add Magnolia CMS + Vercel integration example

--- templates/components/List.js
+++ templates/components/List.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { EditableArea } from '@magnolia/react-editor';
 
-export default function List(props) {
-	const { items } = props;
-
+export default function List({ items, metadata }) {
 	return (
 		<>
 			<div className="hint">[LIST]</div>
 			<ul className="List">
 				<EditableArea
 					content={items}
-					parentTemplateId={props.metadata['mgnl:template']}
+					parentTemplateId={metadata['mgnl:template']}
 				/>
 			</ul>
 		</>
